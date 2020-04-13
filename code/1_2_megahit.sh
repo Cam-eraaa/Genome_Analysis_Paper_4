7a4d98f6a751be5050fad6cbc59686404c799329
Camille <60142899+Cam-eraaa@users.noreply.github.com>
2020-04-13 19:38:07 +0200

    Update 1_2_megahit.sh

--- code/1_2_megahit.sh
+++ code/1_2_megahit.sh
@@ -10,6 +10,6 @@
 module load bioinfo-tools
 module load megahit
 # Your commands
-megahit –kmin-1pass -t 2 -o /home/camil/private/Genome_Analysis_Paper_4/analyses/1_assembly/1_2_metagenome_assembly/DNA_trimmed /proj/g2020008/4_Thrash_2017/DNA_trimmed/*.fastq.gz
-megahit –kmin-1pass -t 2 -o /home/camil/private/Genome_Analysis_Paper_4/analyses/1_assembly/1_2_metagenome_assembly/RNA_untrimmed /proj/g2020008/4_Thrash_2017/RNA_untrimmed/*.fastq.gz
-megahit –kmin-1pass -t 2 -o /home/camil/private/Genome_Analysis_Paper_4/analyses/1_assembly/1_2_metagenome_assembly/RNA_trimmed /home/camil/private/trimmed_RNA/*fastq.gz
+megahit --kmin-1pass -t 2 -o /home/camil/private/Genome_Analysis_Paper_4/analyses/1_assembly/1_2_metagenome_assembly/DNA_trimmed /proj/g2020008/4_Thrash_2017/DNA_trimmed/*.fastq.gz
+megahit --kmin-1pass -t 2 -o /home/camil/private/Genome_Analysis_Paper_4/analyses/1_assembly/1_2_metagenome_assembly/RNA_untrimmed /proj/g2020008/4_Thrash_2017/RNA_untrimmed/*.fastq.gz
+megahit --kmin-1pass -t 2 -o /home/camil/private/Genome_Analysis_Paper_4/analyses/1_assembly/1_2_metagenome_assembly/RNA_trimmed /home/camil/private/trimmed_RNA/*fastq.gz
