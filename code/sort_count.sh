0db87a6b9f1aca27f9bb2b680b88d0200b6a0d79
Camille <60142899+Cam-eraaa@users.noreply.github.com>
2020-05-24 23:08:03 +0200

    Update sort_count.sh

--- code/sort_count.sh
+++ code/sort_count.sh
@@ -1,19 +1,17 @@
 path_to_gff=/home/camil/private/Genome_Analysis_Paper_4/analyses/3_binning_assessment_annotation/3_2_functional_annotation
 path_to_out=/home/camil/private/Genome_Analysis_Paper_4/analyses/5_read_counting
 
-for n in 1 12 27
-do
-	echo "##############################"
-	echo "Bin "${n}
-	echo "##############################"
-	echo "2137"
-	echo "##############################"
-	python3 expr_count.py ${path_to_gff}/archaea/bin_${n}/bin_${n}.gff ${path_to_out}/bin_${n}_count_2137.out
-	echo "##############################"
-	echo "2139"
-	echo "##############################"
-	python3 expr_count.py ${path_to_gff}/archaea/bin_${n}/bin_${n}.gff ${path_to_out}/bin_${n}_count_2139.out
-done
+echo "##############################"
+echo "Bin 1"
+echo "##############################"
+echo "2137"
+echo "##############################"
+python3 expr_count.py ${path_to_gff}/archaea/bin_1/bin_1.gff ${path_to_out}/bin_1_count_2137.out
+echo "##############################"
+echo "2139"
+echo "##############################"
+python3 expr_count.py ${path_to_gff}/archaea/bin_1/bin_1.gff ${path_to_out}/bin_1_count_2139.out
+
 for n in 3 4 5 9 11 13 14 17 20 23 24
 do
 	echo "##############################"
